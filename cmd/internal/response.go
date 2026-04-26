9a782ebaa2ed22e9dcd7ed42523ca6bebe84217a
Ivan-Martins-DevProjects <Ivan_G.Martins@outlook.com>
2026-04-25 21:09:57 -0300

    Alteração no objeto Response
    Lógica alterada para definir respostas de maneira dinâmica

--- cmd/internal/response.go
+++ cmd/internal/response.go
@@ -4,30 +4,36 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"net"
 	"net/http"
 )
 
 type Response struct {
-	Status      int
-	ContentType string
-	Body        any
+	Conn   net.Conn
+	Status int
+	Body   any
 }
 
-func SendResponse(status int, contentType string, body any, w io.Writer) {
-	response := &Response{
-		Status:      status,
-		ContentType: contentType,
-		Body:        body,
+func CreateResponse(c net.Conn) *Response {
+	return &Response{
+		Conn:   c,
+		Status: 0,
+		Body:   nil,
 	}
+}
 
-	response.WriteResponse(w)
+func (r *Response) SendResponse(status int, body any) {
+	r.Status = status
+	r.Body = body
+
+	r.WriteResponse(r.Conn)
 }
 
 func (r *Response) WriteResponse(w io.Writer) {
 	data, _ := json.Marshal(r.Body)
 
 	fmt.Fprintf(w, "HTTP/1.1 %d %s\r\n", r.Status, http.StatusText(r.Status))
-	fmt.Fprintf(w, "Content-Type: %s\r\n", r.ContentType)
+	fmt.Fprintf(w, "Content-Type: application/json\r\n")
 	fmt.Fprintf(w, "Content-Length: %d\r\n", len(data))
 	fmt.Fprintf(w, "Connection: close\r\n")
 	fmt.Fprintf(w, "\r\n")
