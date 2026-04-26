3d37284a40cff9d318a39d7bbfa97b222a97be02
Ivan-Martins-DevProjects <Ivan_G.Martins@outlook.com>
2026-04-25 21:11:29 -0300

    Adicionado lógica para criação dinâmica de endpoints em geral

--- cmd/internal/endpoints.go
+++ cmd/internal/endpoints.go
@@ -1,9 +1,52 @@
 package internal
 
-type Context struct {
-	Request *Request
+import (
+	"net"
+)
+
+type Handler interface {
+	ServeHTTP(*Request, *Response)
 }
 
-func SetEndpoint(path string) {
+type HandlerFunc func(*Request, *Response)
 
+func (f HandlerFunc) ServeHTTP(req *Request, res *Response) {
+	f(req, res)
+}
+
+type Router struct {
+	conn   net.Conn
+	routes map[string]Handler
+}
+
+func CreateRouter(c net.Conn) *Router {
+	return &Router{
+		conn:   c,
+		routes: make(map[string]Handler),
+	}
+}
+
+func (r *Router) AddRoute(path string, handler Handler) {
+	r.routes[path] = handler
+}
+
+func (r *Router) Serve(req *Request, res *Response) {
+	handler, ok := r.routes[req.Params.Path]
+
+	type Result struct {
+		Status  string
+		Message string
+	}
+
+	if !ok {
+		result := &Result{
+			Status:  "Error",
+			Message: "Rota não encontrada",
+		}
+
+		res.SendResponse(404, result)
+		return
+	}
+
+	handler.ServeHTTP(req, res)
 }
