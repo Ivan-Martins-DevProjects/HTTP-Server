7c97ddfa62958d6eb04d73bcef4e770900715568
Ivan-Martins-DevProjects <Ivan_G.Martins@outlook.com>
2026-04-25 21:12:03 -0300

    Lógica de execução do servidor
    Atualmente o servidor encerra toda conexão, keep-alive ainda será
    implementado

--- cmd/internal/server.go
+++ cmd/internal/server.go
@@ -5,42 +5,55 @@ import (
 	"net"
 )
 
-func HandleConnections(c net.Conn, IpList *IpList) {
-	defer c.Close()
+type Content struct {
+	Request *Request
+	IpAddr  string
+}
 
+func GetContext(c net.Conn, ipList *IpList) *Content {
 	req, err := GetRequestContext(c)
 	if err != nil {
-		log.Fatal("error", "error", err)
-		return
+		log.Printf("Erro: %v", err)
+		return nil
 	}
 
 	ipAddr := c.RemoteAddr().String()
-	if err := IpList.AddAndCheckIP(ipAddr); err != nil {
-		log.Fatal("error", "error", err)
-		return
+	if err := ipList.AddAndCheckIP(ipAddr); err != nil {
+		log.Printf("Erro: %v", err)
+		return nil
 	}
 
-	log.Printf("IP %s logado\n", ipAddr)
+	return &Content{
+		Request: req,
+		IpAddr:  ipAddr,
+	}
+}
 
-	SendResponse(
-		200,
-		"application/json",
-		req.Body,
-		c,
-	)
+func HandleConnections(c net.Conn) {
+	defer c.Close()
 
+	ipList := CreateIpList()
+	context := GetContext(c, ipList)
+
+	log.Printf("IP %s logado\n", context.IpAddr)
+
+	router := CreateRouter(c)
+	response := CreateResponse(c)
+
+	router.AddRoute("/api", HandlerFunc(TestResponse))
+	router.AddRoute("/echo", HandlerFunc(EchoResponse))
+
+	router.Serve(context.Request, response)
 }
 
 func Run(listener net.Listener) error {
-	ipList := CreateIpList()
-
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
 			return err
 		}
 
-		go HandleConnections(conn, ipList)
+		go HandleConnections(conn)
 	}
 
 }
